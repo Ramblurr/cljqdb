e06651fc6beb5e7130ff5232c51453a1b4c829c7
Casey Link <unnamedrambler@gmail.com>
2011-05-08 19:18:34 -0400

    custom middleware to handle nginx proxy

--- src/quotes/core.clj
+++ src/quotes/core.clj
@@ -20,14 +20,22 @@
             (str content-type "; charset=" charset)))
         response))))
 
+(defn wrap-remote-addr [app]
+  (fn [request]
+    (let [request (assoc request :real-remote-addr (if (contains? (request :headers) "x-real-ip")
+      ((request :headers) "x-real-ip")
+      (request :remote-addr)))]
+    (app request))))
+
 (defn render [t]
   {:status 200, :headers {"Content-Type" "text/html; charset=utf-8"}, :body t})
 (defroutes main-routes
   (GET "/" [] (render (controller/index)))
+  ;(GET "/debug" req (render (str req)))
   (GET "/quotes/submit" [] (render (controller/quote-form)))
   (GET "/quotes/:id" [id] (render (controller/quote-view id)))
   (GET "/quotes/:id/votes" [id] (controller/votes id))
-  (PUT "/quotes/:id/votes" {remote-addr :remote-addr params :params} (controller/put-votes remote-addr params))
+  (PUT "/quotes/:id/votes" {remote-addr :real-remote-addr params :params} (controller/put-votes remote-addr params))
   (GET "/quotes" {params :params} (render (controller/browse-quotes params)))
   (GET "/top" {params :params} (render (controller/top-quotes params)))
   (GET "/random" {params :params} (render (controller/random-quotes params)))
@@ -39,6 +47,7 @@
 (def app
     (-> main-routes
         compojure.handler/site
+        wrap-remote-addr
         (wrap-charset (:charset "utf8"))
       ))
 
